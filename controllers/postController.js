10609e2b291a9b2b295e2066e649e2fa7acd0668
gvivero1 <gvivero1@uncc.edu>
2024-02-11 01:22:15 -0500

    displays post correctly and the created by field works, created by uses session

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,7 @@ const Post = require("../models/postModels");
 
 async function getAllPosts() {
   try {
-    const posts = await Post.find({}).exec();
+    const posts = await Post.find().populate('createdBy').exec();
     return posts;
   } catch (err) {
     console.error("Error retrieving Posts:", err);
